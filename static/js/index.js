f0b02836b065ed9679ecd0ab24524c489fa2f0b3
Ka3chi <aldrinalmoradie69@gmail.com>
2024-03-07 16:55:59 +0800

    animation

--- static/js/index.js
+++ static/js/index.js
@@ -1,17 +1,21 @@
 $(document).ready(function () {
     //button shrink side nav
     $('#b1').click(function () { 
-        var sidenav = $('#sidenav')
-        // var compared = sidenav.css('width')
+        var sidenav = $('#sidenav');
         
-        if(sidenav.css('width') === "250px"){
-        sidenav.css('width','100px');
-        $('.sidelabel').css('display','none')
-        }else{
-            sidenav.css('width','250px');
-            $('.sidelabel').css('display','block');
+        if (sidenav.css('width') === "250px") {
+            sidenav.css({
+                'width': '100px',
+                // 'transition': 'width 0.3s ease'
+            });
+            $('.sidelabel').css('display', 'none');
+        } else {
+            sidenav.css({
+                'width': '250px',
+                // 'transition': 'width 0.3s ease'
+            });
+            $('.sidelabel').css('display', 'block');
         }
-     
     });
     
 });
